ac839fb220306514698c7cf1c6c898a5d14e421b
mseifert9 <mseifert9@gmail.com>
2017-05-27 02:54:20 -0700

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -1,22 +1,33 @@
-var dependants = [
-	{file: "bar.js", ns: "FooBar", subDir: "foo"},
-	// preload image
-	{file: "image.png", baseDir: "js", subDir: "img"},
-]
-$ms.sourceFiles.add(dependants);
+$ms.sourceFiles.add([
+    /*	hello.js is used for the demo.php
+     *	this file has two dependants which will be automatically queued for loading
+     *	    1.	/js/foo/bar.js - which supplies $ms.FooBar.secretMessage()
+     *	    2.	/img/star-18.png - which requires the document 'load' event to fire before the image is appended to the docuement
+     */
+    {file: "bar.js", ns: "FooBar", subDir: "foo"},
+    // preload image
+    {file: "star-18.png", dependencies:[{file: function onload(){}}]}
+]);
 $ms.sourceFiles.load();
 
 
 $ms.Hello = {
-  sayHello: function(text){
-    var label = document.createElement("label");
-    document.body.appendChild(label);
-  	var foobar = $ms.FooBar.secretMessage;
-    label.innerText = text + " - " + foobar;
-    
-    var img = document.createElement("img");
-    img.src = $ms.STATIC_JS_COMMON + "/img/star-18.png"
-    document.body.appendChild(img);
-  }
+    sayHello: function(text){
+	var label = document.createElement("label");
+	document.body.appendChild(label);
+	label.innerText = text;
+	label.onclick = function(){
+	    // $ms.FooBar.secretMessage() is from a dependent js file
+	    var foobar = $ms.FooBar.secretMessage();
+	    label.innerText = text + " - " + foobar;
+
+	    if (!document.getElementById("star")){
+		var img = document.createElement("img");
+		img.id = "star";
+		img.src = $ms.STATIC_IMG_COMMON + "/star-18.png"
+		document.body.appendChild(img);
+	    }
+	}    
+    }
 }
 
